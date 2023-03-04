39b75cdbdd4baec64d304b0abcb06ca4c92c86ff
rob.tisdale <rtisdale.bt@gmail.com>
2023-03-03 18:35:39 -0600

    Full-Stack Spring test

--- src/main/java/com/skilldistillery/film/data/FilmDAO.java
+++ src/main/java/com/skilldistillery/film/data/FilmDAO.java
@@ -1,5 +1,19 @@
 package com.skilldistillery.film.data;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import com.skilldistillery.film.entities.Actor;
+import com.skilldistillery.film.entities.Film;
+
 public interface FilmDAO {
 
+	  public Film findFilmById(int filmId) throws SQLException;
+	  public List<Actor> findActorsByFilmId(int filmId);
+	  public List<Film> findFilmByKeywordSearch(String keyword);
+	  public Actor findActorById(int actorId) throws SQLException;
+	  
+	//  public boolean saveFilm(Film film);
+	//  public Film createFilm(Film film);
+	//  public boolean deleteFilm(Film film);
 }
---
+++ src/main/webapp/WEB-INF/home.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Film Site</title>
+</head>
+<body>
+<h1>Welcome To The ThunderDome!</h1>
+</body>
+</html>
